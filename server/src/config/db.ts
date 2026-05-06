801539899a9df8e4f94f9dba184bf853c4a3d30b
yashika <yashika.agarwal@unthinkable.co>
2026-05-06 15:55:39 +0530

    fixes

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,20 @@
 import mongoose from 'mongoose';
 import { env } from './env.js';
 
+mongoose.connection.on('disconnected', () => {
+  console.error('MongoDB disconnected');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('MongoDB error:', err);
+});
+
 export async function connectDB(): Promise<void> {
-  await mongoose.connect(env.MONGO_CONNECTION_STRING);
-  console.log('MongoDB connected');
+  try {
+    await mongoose.connect(env.MONGO_CONNECTION_STRING);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection failed:', err);
+    process.exit(1);
+  }
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,18 +2,22 @@ import app from './app.js';
 import { env } from './config/env.js';
 import { connectDB } from './config/db.js';
 
-await connectDB();
+const start = async () => {
+  await connectDB(); // DB first, then listen
+  
+  const server = app.listen(env.PORT, () => {
+    console.log(`Server running on port ${env.PORT} in ${env.NODE_ENV} mode`);
+  });
 
-const server = app.listen(env.PORT, () => {
-  console.log(`Server running on port ${env.PORT} in ${env.NODE_ENV} mode`);
-});
+  process.on('unhandledRejection', (err) => {
+    console.error('Unhandled Rejection:', err);
+    server.close(() => process.exit(1));
+  });
 
-process.on('unhandledRejection', (err) => {
-  console.error('Unhandled Rejection:', err);
-  server.close(() => process.exit(1));
-});
+  process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+  });
+};
 
-process.on('uncaughtException', (err) => {
-  console.error('Uncaught Exception:', err);
-  process.exit(1);
-});
+start();
